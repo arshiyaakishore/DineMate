c570734e07e941e779fcecbec1b361236013d76f
arshiyaakishore <arshiyakishore@gmail.com>
2024-10-13 10:26:09 +0530

    almost done

--- generic_helper.py
+++ generic_helper.py
@@ -1,4 +1,3 @@
-
 import re
 
 def get_str_from_food_dict(food_dict: dict):
@@ -6,10 +5,11 @@ def get_str_from_food_dict(food_dict: dict):
     return result
 
 
+
 def extract_session_id(session_str: str):
     match = re.search(r"/sessions/(.*?)/contexts/", session_str)
     if match:
-        extracted_string = match.group(0)
+        extracted_string = match.group(1)  # Changed from group(0) to group(1)
         return extracted_string
 
     return ""
